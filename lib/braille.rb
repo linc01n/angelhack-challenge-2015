72f3d9669a0743fdd4c0806df3959d6f61648529
Lincoln Lee <git@lincoln.hk>
2015-06-17 18:53:58 +0800

    Implement convert method

--- lib/braille.rb
+++ lib/braille.rb
@@ -10,9 +10,20 @@ class Braille
 ': "a"
     }
 
+    @char_array = []
   end
 
   def parse(input)
     @rules[input.to_sym]
+  end
+
+  def convert(input_string)
+    input_string.split("\n").each do |line|
+      line.split(" ").each_with_index do |char, index|
+        @char_array[index] ||= []
+        @char_array[index] << char + "\n"
+      end
+    end
+    @char_array.map!{|char| char.join}
   end
 end
