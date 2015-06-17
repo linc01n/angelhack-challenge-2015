dd3034744cb006581cf80a2af116c05bb143a82e
Lincoln Lee <git@lincoln.hk>
2015-06-17 18:46:48 +0800

    Add test for convert input string to array

--- test/lib/Braille/test_braille.rb
+++ test/lib/Braille/test_braille.rb
@@ -13,10 +13,19 @@ O.
 ..
 EOF
 
+    @braille_for_helloword = <<-EOF
+O. O. O. O. O. .O O. O. O. OO
+OO .O O. O. .O OO .O OO O. .O
+.. .. O. O. O. .O O. O. O. ..
+EOF
   end
 
   def test_convert_braille_char_for_a
     assert_equal 'a', @braille.parse(@braille_for_a)
+  end
+
+  def test_convert_input_string_into_array_of_2x3
+    assert_equal "O.\nOO\n..\n", @braille.convert(@braille_for_helloword)[0]
   end
 
 end
